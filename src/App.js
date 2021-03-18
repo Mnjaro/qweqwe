1595eb80b36bfed02f8d0bffdae4acf1f2820d8d
Mnjaro <grt.aria@gmail.com>
2021-03-18 17:20:17 +0100

    ed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const submit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://matchup-be.herokuapp.com/users/login", {
+        username: "Hermann",
+        password: "123",
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => console.log("error", e));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <input type="text" name="username" />
+        <input type="text" name="password" />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
